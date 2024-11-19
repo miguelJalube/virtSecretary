3b8ce7fe0c7b46211588a04ea587afbee6119320
miguelJalube <miguel.jalube@gmail.com>
2024-11-19 16:13:06 +0100

    added chat history and changed template

--- src/app.py
+++ src/app.py
@@ -3,30 +3,32 @@ import sys
 import logging
 from flask import Flask, redirect, request, jsonify, render_template, stream_with_context, Response
 
-from llama_index.core import Settings
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.core import (
+    Settings,
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    load_index_from_storage,
+    StorageContext,
+)
 from llama_index.llms.ollama import Ollama
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core.memory import ChatMemoryBuffer
 
-import logging
 
-from llama_index.core import (
-    load_index_from_storage,
-    StorageContext,
-)
+import logging
 
 # Set up logging
 logger = logging.getLogger(__name__)
 
-LLM = os.environ.get("LLM", "llama3.2:1b")
-LLM_SERVER = os.environ.get("LLM_SERVER", "http://ollama:11434")
-PORT = os.environ.get("PORT", 8071)
-EMBED_MODEL = os.environ.get("EMBED_MODEL", "intfloat/multilingual-e5-large")
+LLM =           os.environ.get("LLM", "llama3.2:1b")
+LLM_SERVER =    os.environ.get("LLM_SERVER", "http://ollama:11434")
+PORT =          os.environ.get("PORT", 8071)
+EMBED_MODEL =   os.environ.get("EMBED_MODEL", "intfloat/multilingual-e5-large")
 
 # Initialize Flask application
 app = Flask(__name__)
 
+chat_engine = None
 
 # Index the data
 @app.route("/index")
@@ -52,29 +54,8 @@ def index():
 # Chat route for handling POST requests with user queries
 @app.route("/chat", methods=["POST"])
 def chat():
-    # rebuild storage context
-    storage_context = StorageContext.from_defaults(
-        persist_dir="src/storage"
-    )
-    
-    try:
-        # load index
-        index = load_index_from_storage(
-            storage_context,
-            index_id="vector_index"
-        )
-    except Exception as e:
-        message = f"Error loading index: {str(e)}"
-        logging.error(message)
-    
     data = request.get_json()
     
-    memory = ChatMemoryBuffer.from_defaults(llm=Settings.llm, token_limit=1500)
-    
-    chat_engine = index.as_chat_engine(
-        chat_mode="context",
-        memory = memory
-    )
     logger.warning("Data : " + str(data["query"]))
     
     streaming_response = chat_engine.stream_chat(data["query"])
@@ -87,8 +68,6 @@ def chat():
 def home():
     logging.info("Rendering home page")
     
-    # redirect to chat page with a query "presente toi"
-    #return redirect("/chat?query=presente%20toi")
     
     return render_template("index.html")
 
@@ -106,6 +85,28 @@ if __name__ == "__main__":
         model_name=EMBED_MODEL,
         trust_remote_code=True,
         cache_folder="cache"
+    )
+    
+    # rebuild storage context
+    storage_context = StorageContext.from_defaults(
+        persist_dir="src/storage"
+    )
+    
+    try:
+        # load index
+        index = load_index_from_storage(
+            storage_context,
+            index_id="vector_index"
+        )
+    except Exception as e:
+        message = f"Error loading index: {str(e)}"
+        logging.error(message)
+    
+    memory = ChatMemoryBuffer.from_defaults(llm=Settings.llm, token_limit=1500)
+    
+    chat_engine = index.as_chat_engine(
+        chat_mode="context",
+        memory = memory
     )
     
     app.run(debug=True, host="0.0.0.0", port=PORT)
